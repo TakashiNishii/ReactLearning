327b7863909ce74fcd1d00780e93206034699b61
TakashiNishii <ygortn14@gmail.com>
2023-01-06 21:07:06 -0300

    :art: Intro e desafio terminados

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World React</h1>
+      <h2>Emmet  funcionou</h2>
+      <h3>Desafio proposto é desafio cumprido</h3>
     </div>
   );
 }

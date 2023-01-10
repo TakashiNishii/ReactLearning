05adf7b5150e35694ff65303bfb82cb35d2a8497
TakashiNishii <ygortn14@gmail.com>
2023-01-09 23:39:16 -0300

    :art: Importação do componente

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,9 +1,14 @@
+//components
+import FirstComponent from './components/FirstComponent';
+
+// Styles / CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent />
     </div>
   );
 }

cccfea1568c4fa6e189f31c9bf644afceaa051c8
TakashiNishii <ygortn14@gmail.com>
2023-03-13 16:29:34 -0300

    :art: Comentario de JSX

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,7 +1,17 @@
+// Exemplo de comentário em JSX
+
 const FirstComponent = () => {
-    return (
+    // Fora do return, o comentário é um comentário de JS
+    
+    /*
+        Comentário de bloco
+    */
+
+    return (      
         <div>
+            { /* Comentário dentro do return é um comentário de JSX e ele é feito assim*/}
             <h1>Meu primeiro componente</h1>
+            
             <p className="teste">Meu texto</p>
         </div>
     );

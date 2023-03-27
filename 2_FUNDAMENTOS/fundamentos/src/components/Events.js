c3b8597fe2768f1a5e159ef7776a1ed22fc09cc1
TakashiNishii <ygortn14@gmail.com>
2023-03-27 17:12:17 -0300

    :art: Event functions inline

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -10,6 +10,19 @@ const Events = () => {
             <div>
                 <button onClick={handleMyEvent}>Clique aqui!</button>
             </div>
+            {/* Não recomendado usar as funções no event como abaixo: */}
+            <div>
+                <button onClick={() => console.log("Clicou!")}>Clique aqui também!</button>
+                <button 
+                    onClick={() => { 
+                        if(true){
+                            console.log("Isso não deveria existir =)")
+                        }
+                    }}
+                >
+                    Clica aqui, por favor
+                </button>
+            </div>
         </div>
     )
 }

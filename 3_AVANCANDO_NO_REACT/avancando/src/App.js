853b7c0a05dbf3651f1b7df5692a6d66a1cccb05
TakashiNishii <ygortn14@gmail.com>
2023-04-20 19:42:12 -0300

    :art: Imagens públicas

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
     </div>
   );
 }

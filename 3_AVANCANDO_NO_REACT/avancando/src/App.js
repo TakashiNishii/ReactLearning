613165d69d356a286b67109e7fd0e036b0c2f5d7
TakashiNishii <ygortn14@gmail.com>
2023-04-20 20:13:39 -0300

    :art: Image by src/assts

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 
+import City from './assets/city.jpg';
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +10,10 @@ function App() {
       {/* Imagem em public */}
       <div>
         <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem em asset */}
+      <div>
+        <img src={City} alt="Cidade" />
       </div>
     </div>
   );

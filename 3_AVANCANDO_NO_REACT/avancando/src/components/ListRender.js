6fa43a9eb9256044c6457646918ec73d8daf42b0
TakashiNishii <ygortn14@gmail.com>
2023-05-03 20:03:37 -0300

    :rocket: Propriedade key

--- 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.js
@@ -2,13 +2,27 @@ import { useState } from 'react'
 
 export const ListRender = () => {
     const [list] = useState(["Evaldo", "Guilda", "Felipe", "Takashi"])
+    const [users] = useState([
+        { id: 1, name: "Evaldo", age: 30 },
+        { id: 2, name: "Guilda", age: 29 },
+        { id: 3, name: "Felipe", age: 28 },
+        { id: 4, name: "Takashi", age: 27 },
+    ])
     return (
         <div>
             <ul>
-                {list.map((item) => (
-                    <li key={item}>{item}</li>
+                {list.map((item, index) => (
+                    <li key={index}>{item}</li>
                 ))}
             </ul>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.name} - {user.age}
+                    </li>
+                ))}
+            </ul>
+
         </div>
     )
 }

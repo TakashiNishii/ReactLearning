1cc5c83fb794f33527af937551d4264c822342bb
TakashiNishii <ygortn14@gmail.com>
2023-05-15 19:07:38 -0300

    :wrench: If e else no JSX

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 
 import City from './assets/city.jpg';
+import ConditionalRender from './components/ConditionalRender';
 import { ListRender } from './components/ListRender';
 import { ManageData } from './components/ManageData';
 
@@ -19,6 +20,7 @@ function App() {
       </div>
       <ManageData />
       <ListRender />
+      <ConditionalRender />
     </div>
   );
 }
--- 3_AVANCANDO_NO_REACT/avancando/src/components/ConditionalRender.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/components/ConditionalRender.js
@@ -2,11 +2,24 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
     const [x] = useState(true)
+    const [name, setName] = useState("Taka")
     return (
         <div>
             <h1>Isso será exibido?</h1>
             {x && <p>Se x for true, sim!</p>}
             {!x && <p>Agora x é falso</p>}
+            <h1>If ternário</h1>
+            {name === "Takashi" ?
+                (
+                    <div>
+                        <p>O nome é Takashi</p>
+                    </div>
+                ) : (
+                    <div>
+                        <p>Nome não encontrado</p>
+                    </div>
+                )}
+            <button onClick={() => setName("Takashi")}>Clica aqui</button>
         </div>
     )
 }

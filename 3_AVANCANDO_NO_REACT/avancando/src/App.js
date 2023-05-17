fe7cc7324230ecf1f18827a766c1890d1a4af607
TakashiNishii <ygortn14@gmail.com>
2023-05-17 15:25:51 -0300

    :wrench: Props

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -1,12 +1,15 @@
 
+import { useState } from 'react';
 import './App.css';
 
 import City from './assets/city.jpg';
 import ConditionalRender from './components/ConditionalRender';
 import { ListRender } from './components/ListRender';
 import { ManageData } from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+  const [userName] = useState("Takashi");
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -21,6 +24,7 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      <ShowUserName name={userName} />
     </div>
   );
 }

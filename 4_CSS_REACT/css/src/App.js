c0f130665342445e71a77e8c55a0e181ce5ab197
TakashiNishii <ygortn14@gmail.com>
2023-06-01 15:33:01 -0300

    :art: CSS de componente

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
+      {/* CSS global */}
       <h1>React com CSS</h1>
+      {/* CSS de componente */}
+      <MyComponent />
+      <p>Este parágrafo é do APP.js</p>
     </div>
   );
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import "./MyComponent.css";
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>CSS de componente</h1>
+      <p>Este é o paragrafo do componente</p>
+      <p className="my-comp-paragraph">Este também é do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;

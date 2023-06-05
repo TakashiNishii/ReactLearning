94017c8913eb8f97d7ed36cfd83274551586043b
TakashiNishii <ygortn14@gmail.com>
2023-06-05 12:48:23 -0300

    :wrench: Label envolvendo input (forma recomendada na documentação)

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -9,6 +9,11 @@ const MyForm = () => {
           <label htmlFor="name">Nome:</label>
           <input type="text" name="name" placeholder="Digite o seu nome" />
         </div>
+        {/* 2 - label envolvendo input */}
+        <label>
+          <span>E-mail:</span>
+          <input type="email" name="email" placeholder="Digite o seu e-mail" />
+        </label>
         <input type="submit" value="Enviar" />
       </form>
     </div>

fce00d52c0070d7687dcfd0839f82b10ab625a88
TakashiNishii <ygortn14@gmail.com>
2023-06-08 15:45:18 -0300

    :rocket: Start Screen

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import StartScreen from "./components/StartScreen";
 
 function App() {
   return (
     <div className="App">
-      <h2>Secret Words</h2>
+      <StartScreen />
     </div>
   );
 }

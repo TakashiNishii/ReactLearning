13653a19b3ab56ab658435ed9ac3c23dc8682220
TakashiNishii <ygortn14@gmail.com>
2023-07-10 15:26:57 -0300

    :art: Game over screen

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -125,7 +125,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === "end" && <GameOver retry={retry} />}
+      {gameStage === "end" && <GameOver retry={retry} score={score} />}
     </div>
   );
 }

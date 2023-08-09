c54c16c403a830fd0a79701aa6edbdf9262fead6
TakashiNishii <ygortn14@gmail.com>
2023-08-09 15:14:20 -0300

    :rocket: Resgatando dados da API

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,10 +1,35 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState, useEffect } from "react";
 
+const url = "http://localhost:3000/products";
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1 - Resgatando dados da API
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price.toFixed(2)}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

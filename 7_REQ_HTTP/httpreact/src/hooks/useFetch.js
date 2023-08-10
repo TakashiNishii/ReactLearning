8b0a90aca8f1584672a4ac8746cf5b9a05617320
TakashiNishii <ygortn14@gmail.com>
2023-08-10 19:03:43 -0300

    :rocket: Custom hooks

---
+++ 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+// 4- Custom hook
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const json = await res.json();
+      setData(json);
+    };
+    fetchData();
+  }, [url]);
+
+  return { data };
+};

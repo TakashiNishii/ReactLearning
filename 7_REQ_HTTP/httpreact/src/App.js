b00e95232fd45b9d8298c59a0c89166a5e9197bd
TakashiNishii <ygortn14@gmail.com>
2023-08-14 15:06:51 -0300

    :wrench: Refactoring POST method

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   // 4 - Custom hook
-  const { data: items } = useFetch(url);
+  const { data: items, httpConfig } = useFetch(url);
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
@@ -36,18 +36,21 @@ function App() {
       price,
     };
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(product),
-    });
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(product),
+    // });
 
-    // 3 - Carregamento dinâmico
-    const addedProduct = await res.json();
+    // // 3 - Carregamento dinâmico
+    // const addedProduct = await res.json();
 
-    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    // 5 - Refatorando post
+    httpConfig(product, "POST");
 
     setName("");
     setPrice("");
--- 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
+++ 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
@@ -4,6 +4,25 @@ import { useState, useEffect } from "react";
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
 
+  // 5 - Refatorando POST
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setMethod(method);
+    }
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(url);
@@ -11,7 +30,22 @@ export const useFetch = (url) => {
       setData(json);
     };
     fetchData();
-  }, [url]);
+  }, [url, callFetch]);
 
-  return { data };
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+
+        const res = await fetch(...fetchOptions);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+      }
+    };
+    httpRequest();
+  }, [config, method, url]);
+
+  return { data, httpConfig };
 };

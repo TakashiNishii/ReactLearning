acaede16373faf150bcf292277d006f4f1ed3447
TakashiNishii <ygortn14@gmail.com>
2023-08-21 16:03:37 -0300

    :wrench: Link tag

--- 8_REACT_ROUTER/reactrouter/src/App.js
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <h1>React Router</h1>
 
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link>
+    </nav>
+  );
+};
+
+export default Navbar;

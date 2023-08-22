52356ee25c7363bad83e09e237e4b8bb796bcf5e
TakashiNishii <ygortn14@gmail.com>
2023-08-22 01:58:29 -0300

    :art: NavLink component, how to use

--- 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
+++ 8_REACT_ROUTER/reactrouter/src/components/Navbar.js
@@ -1,12 +1,21 @@
 // 2 - Links com React Router
 import "./Navbar.css";
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">Sobre</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link> */}
+      <NavLink
+        to="/"
+        // Para controlar se o link está ativo ou não (exclusivo do NavLink):
+
+        // className={({ isActive }) => (isActive ? "esta-ativo" : "nao-ativo")}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">Sobre</NavLink>
     </nav>
   );
 };

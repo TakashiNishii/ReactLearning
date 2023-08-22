29ed787ea9cbfcc4dd0b5d94134e1298e3259293
TakashiNishii <ygortn14@gmail.com>
2023-08-22 00:35:01 -0300

    :wrench: Dinamic loading data

--- 8_REACT_ROUTER/reactrouter/src/pages/Product.js
+++ 8_REACT_ROUTER/reactrouter/src/pages/Product.js
@@ -1,9 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 const Product = () => {
   const { id } = useParams();
+
+  const url = `http://localhost:3000/products/${id}`;
+
+  const { data: product, loading, error } = useFetch(url);
+
+  console.log(product);
   return (
     <>
       <p>ID do produto: {id}</p>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R$ {product.price}</p>
+        </div>
+      )}
     </>
   );
 };

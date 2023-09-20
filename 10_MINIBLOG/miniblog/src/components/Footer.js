8cb90ac55fc4b9328a30db530c39e2608c14f70e
TakashiNishii <ygortn14@gmail.com>
2023-09-20 15:30:03 -0300

    :art: CSS Navbar and footer

--- 10_MINIBLOG/miniblog/src/components/Footer.js
+++ 10_MINIBLOG/miniblog/src/components/Footer.js
@@ -1,7 +1,7 @@
 import styles from "./Footer.module.css";
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Escreva sobre o que você tem interesse!</h3>
       <p>Mini Blog &copy; 2023</p>
     </footer>

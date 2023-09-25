d47dd7ba2cfe0197c4eea5c45cc423f35b94dba2
TakashiNishii <ygortn14@gmail.com>
2023-09-25 15:12:47 -0300

    :rocket: Create auth hook

---
+++ 10_MINIBLOG/miniblog/src/hooks/useAuthentication.js
@@ -0,0 +1,18 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
+
+import { useState, useEffect } from "react";
+
+export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  //cleanup
+  // deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+};

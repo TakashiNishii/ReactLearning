b98a03ddde179fba508fbbffe5215581fc4d9dbd
TakashiNishii <ygortn14@gmail.com>
2023-10-12 18:17:13 -0300

    :rocket: Login function

--- 10_MINIBLOG/miniblog/src/hooks/useAuthentication.js
+++ 10_MINIBLOG/miniblog/src/hooks/useAuthentication.js
@@ -69,6 +69,32 @@ export const useAuthentication = () => {
     signOut(auth);
   };
 
+  // Login - sign in
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -79,5 +105,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
   };
 };
--- 10_MINIBLOG/miniblog/src/pages/Login/Login.js
+++ 10_MINIBLOG/miniblog/src/pages/Login/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const { createUser, error: authError, loading } = useAuthentication();
+  const { login, error: authError, loading } = useAuthentication();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,7 +21,7 @@ const Login = () => {
       password,
     };
 
-    const res = await createUser(user);
+    const res = await login(user);
 
     console.log(user);
   };

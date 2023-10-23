944959b3cad790b25091f8b58567953394f27e59
TakashiNishii <ygortn14@gmail.com>
2023-10-23 18:18:55 -0300

    :rocket: Exibindo posts na home

---
+++ 10_MINIBLOG/miniblog/src/components/PostDetail.js
@@ -0,0 +1,26 @@
+import styles from "./PostDetail.module.css";
+
+import { Link } from "react-router-dom";
+
+const PostDetail = ({ post }) => {
+  return (
+    <div>
+      <img src={post.image} alt={post.title} />
+      <h2>{post.title}</h2>
+      <p>{post.createdBy}</p>
+      <div>
+        {post.tagsArray.map((tag) => (
+          <p>
+            <span>#</span>
+            {tag}
+          </p>
+        ))}
+      </div>
+      <Link to={`/posts/${post.id}`} className="btn btn-outline">
+        Ler
+      </Link>
+    </div>
+  );
+};
+
+export default PostDetail;
--- 10_MINIBLOG/miniblog/src/pages/Home/Home.js
+++ 10_MINIBLOG/miniblog/src/pages/Home/Home.js
@@ -4,12 +4,14 @@ import styles from "./Home.module.css";
 // Hooks
 import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocument";
+import PostDetail from "../../components/PostDetail";
 
 // Components
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,7 +28,8 @@ const Home = () => {
         <button className="btn btn-dark">Pesquisar</button>
       </form>
       <div>
-        <h1>Posts...</h1>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>

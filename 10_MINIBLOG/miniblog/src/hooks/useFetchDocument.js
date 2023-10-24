31cac4c0af942e3ca39e3c2ab23fad5d04611d59
TakashiNishii <ygortn14@gmail.com>
2023-10-24 15:41:32 -0300

    :rocket: Exibindo resultados da busca

--- 10_MINIBLOG/miniblog/src/hooks/useFetchDocument.js
+++ 10_MINIBLOG/miniblog/src/hooks/useFetchDocument.js
@@ -30,7 +30,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
         if (search) {
           q = await query(
             collectionRef,
-            where("tags", "array-contains", search),
+            where("tagsArray", "array-contains", search),
             orderBy("createdAt", "desc")
           );
         } else {

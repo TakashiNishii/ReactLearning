6500d8663957296d261c946a508d06405ddcca6f
TakashiNishii <ygortn14@gmail.com>
2023-10-26 20:34:13 -0300

    :art: CSS do dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -19,7 +19,7 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h1>Dashboard</h1>
       <p>Gerencie os seus posts</p>
       {posts && posts.length === 0 ? (
@@ -31,14 +31,14 @@ const Dashboard = () => {
         </div>
       ) : (
         <>
-          <div>
+          <div className={styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
           </div>
 
           {posts &&
             posts.map((post) => (
-              <div key={post.id}>
+              <div key={post.id} className={styles.post_row}>
                 <p>{post.title}</p>
                 <div>
                   <Link to={`/posts/${post.id}`} className="btn btn-outline">

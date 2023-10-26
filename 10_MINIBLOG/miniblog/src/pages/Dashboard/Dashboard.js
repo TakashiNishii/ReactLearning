ce6aff9f26374164c403c8e96fdfb95672cb4b9d
TakashiNishii <ygortn14@gmail.com>
2023-10-26 17:33:05 -0300

    :construction: Estruturando dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,34 @@
 import styles from "./Dashboard.module.css";
+
+import { Link } from "react-router-dom";
+
+//hooks
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.div;
+
+  //posts do usuario
+  const posts = [];
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
     </div>
   );
 };

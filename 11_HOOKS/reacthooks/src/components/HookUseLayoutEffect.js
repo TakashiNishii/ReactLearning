beb59ef3a65573c8830de020c2595afcc47bfeff
TakashiNishii <ygortn14@gmail.com>
2023-11-02 20:25:26 -0300

    :rocket: useLayoutEffect

---
+++ 11_HOOKS/reacthooks/src/components/HookUseLayoutEffect.js
@@ -0,0 +1,26 @@
+import { useEffect, useState, useLayoutEffect } from "react";
+
+const HookUseLayoutEffect = () => {
+  const [name, setName] = useState("Algum nome");
+
+  useEffect(() => {
+    console.log("2");
+    setName("Mudou de novo!");
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log("1");
+    setName("Outro nome");
+  }, []);
+
+  console.log(name);
+  return (
+    <div>
+      <h2>useLayoutEffect</h2>
+      <p>Nome: {name}</p>
+      <hr />
+    </div>
+  );
+};
+
+export default HookUseLayoutEffect;
--- 11_HOOKS/reacthooks/src/pages/Home.js
+++ 11_HOOKS/reacthooks/src/pages/Home.js
@@ -8,6 +8,7 @@ import HookUseEffect from "../components/HookUseEffect";
 import HookUseRef from "../components/HookUseRef";
 import HookUseCallback from "../components/HookUseCallback";
 import HookUseMemo from "../components/HookUseMemo";
+import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
 
 const Home = () => {
   const { contextValue } = useContext(SomeContext);
@@ -22,6 +23,7 @@ const Home = () => {
       <HookUseRef />
       <HookUseCallback />
       <HookUseMemo />
+      <HookUseLayoutEffect />
     </div>
   );
 };

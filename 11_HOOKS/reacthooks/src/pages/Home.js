c39f0a104324feb69dcce599fe447ef4cc5f7163
TakashiNishii <ygortn14@gmail.com>
2023-11-02 04:26:25 -0300

    :rocket: useRef

--- 11_HOOKS/reacthooks/src/pages/Home.js
+++ 11_HOOKS/reacthooks/src/pages/Home.js
@@ -1,11 +1,11 @@
-import React from "react";
-import HookUseState from "../components/HookUseState";
-import HookUseReducer from "../components/HookUseReducer";
-import HookUseEffect from "../components/HookUseEffect";
-
 //useContext
 import { useContext } from "react";
 import { SomeContext } from "../components/HookUseContext";
+
+import HookUseState from "../components/HookUseState";
+import HookUseReducer from "../components/HookUseReducer";
+import HookUseEffect from "../components/HookUseEffect";
+import HookUseRef from "../components/HookUseRef";
 
 const Home = () => {
   const { contextValue } = useContext(SomeContext);
@@ -17,6 +17,7 @@ const Home = () => {
       <h2>useContext</h2>
       <p>Valor do context: {contextValue}</p>
       <hr />
+      <HookUseRef />
     </div>
   );
 };

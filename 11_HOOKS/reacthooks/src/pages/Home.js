34017fa40ffd3e9c1fd5c878e44587d7ad172cc6
TakashiNishii <ygortn14@gmail.com>
2023-11-06 20:10:56 -0300

    :wrench: Custom Hook

--- 11_HOOKS/reacthooks/src/pages/Home.js
+++ 11_HOOKS/reacthooks/src/pages/Home.js
@@ -10,6 +10,7 @@ import HookUseCallback from "../components/HookUseCallback";
 import HookUseMemo from "../components/HookUseMemo";
 import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
 import HookUseImperativeHandle from "../components/HookUseImperativeHandle";
+import HookCustom from "../components/HookCustom";
 
 const Home = () => {
   const { contextValue } = useContext(SomeContext);
@@ -26,6 +27,7 @@ const Home = () => {
       <HookUseMemo />
       <HookUseLayoutEffect />
       <HookUseImperativeHandle />
+      <HookCustom />
     </div>
   );
 };

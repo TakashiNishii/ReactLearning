479cd995fe3fe9dfa9d9be18c40d62c69a4e5d19
TakashiNishii <ygortn14@gmail.com>
2023-11-08 17:13:44 -0300

    :wrench: Add .env and config .gitignore

--- 12_REACTGRAM/backend/app.js
+++ 12_REACTGRAM/backend/app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT;
 
 const app = express();
 

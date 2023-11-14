76ff3392be3ccc57164a7fa95a37e3cb6b33ba2f
TakashiNishii <ygortn14@gmail.com>
2023-11-14 15:04:27 -0300

    :wrench: Importando middlewares

--- 12_REACTGRAM/backend/app.js
+++ 12_REACTGRAM/backend/app.js
@@ -12,6 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+// Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// DB connection
+require("./config/db.js");
+
 // routes
 const router = require("./routes/Router.js");
 

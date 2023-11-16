6a0ecf06a4ff311878268a1e9848e0530636e165
TakashiNishii <ygortn14@gmail.com>
2023-11-16 16:07:56 -0300

    :construction: Criando controller do usuário

--- 12_REACTGRAM/backend/routes/Router.js
+++ 12_REACTGRAM/backend/routes/Router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express();
 
+router.use("/api/users", require("./UserRoutes"));
+
 //test route
 router.get("/", (req, res) => {
   res.send("API Working!");

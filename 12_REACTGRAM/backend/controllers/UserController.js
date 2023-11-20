8323c6902d2b919af0b058b08694d321f5e6e5d3
TakashiNishii <ygortn14@gmail.com>
2023-11-20 17:17:52 -0300

    :rocket: Registro de usuário

--- 12_REACTGRAM/backend/controllers/UserController.js
+++ 12_REACTGRAM/backend/controllers/UserController.js
@@ -14,7 +14,37 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  // Check if user already exists
+  const user = await User.findOne({ email });
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, utilize outro e-mail."] });
+  }
+
+  // Generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // Create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  // If user was created successfully, return the token
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
 };
 
 module.exports = {

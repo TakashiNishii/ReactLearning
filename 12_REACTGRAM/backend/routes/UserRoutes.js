d0c96e36db54f74c4a5a66cc87522c8b70f97733
TakashiNishii <ygortn14@gmail.com>
2023-11-20 16:33:31 -0300

    :wrench: Validações de criação de usuário

--- 12_REACTGRAM/backend/routes/UserRoutes.js
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -6,8 +6,9 @@ const { register } = require("../controllers/UserController");
 
 // Middlewares
 const validate = require("../middlewares/handleValidation");
+const { userCreateValidation } = require("../middlewares/userValidation");
 
 // Routes
-router.post("/register", validate, register);
+router.post("/register", userCreateValidation(), validate, register);
 
 module.exports = router;

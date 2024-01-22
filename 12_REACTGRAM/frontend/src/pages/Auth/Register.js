6e4a1b9f604b6a2d2c7b920146166bdf7d0d0f4e
TakashiNishii <ygortn14@gmail.com>
2024-01-22 15:56:11 -0300

    :construction: Register form

--- 12_REACTGRAM/frontend/src/pages/Auth/Register.js
+++ 12_REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -1,7 +1,32 @@
 import "./Auth.css";
 
+// Components
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useState, useEffect } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;

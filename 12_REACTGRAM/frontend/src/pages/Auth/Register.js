14257a53fb5e4905885c2085fe23033ea4ae3183
TakashiNishii <ygortn14@gmail.com>
2024-01-22 16:39:56 -0300

    :art: Auth CSS

--- 12_REACTGRAM/frontend/src/pages/Auth/Register.js
+++ 12_REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -12,7 +12,7 @@ const Register = () => {
   };
 
   return (
-    <div>
+    <div id="register">
       <h2>ReactGram</h2>
       <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
       <form onSubmit={handleSubmit}>

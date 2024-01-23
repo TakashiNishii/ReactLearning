107a35dbb6b510e0a9457bfdcf6486de43e39b1e
TakashiNishii <ygortn14@gmail.com>
2024-01-23 16:12:20 -0300

    :wrench: Add Redux

--- 12_REACTGRAM/frontend/src/index.js
+++ 12_REACTGRAM/frontend/src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// Redux
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

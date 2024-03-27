d2bffea429ac1061b9acf5aeeaff772b7835970a
TakashiNishii <ygortn14@gmail.com>
2024-03-27 17:19:02 -0300

    :wrench: Login slice

--- 12_REACTGRAM/frontend/src/slices/authSlice.js
+++ 12_REACTGRAM/frontend/src/slices/authSlice.js
@@ -25,6 +25,18 @@ export const register = createAsyncThunk(
   }
 );
 
+// Sign in an user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  const data = await authService.login(user);
+
+  //  check for errors
+  if (data.errors) {
+    return thunkAPI.rejectWithValue(data.errors[0]);
+  }
+
+  return data;
+});
+
 // Logout an user
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
@@ -61,6 +73,21 @@ export const authSlice = createSlice({
         state.loading = false;
         state.success = true;
         state.error = null;
+        state.user = null;
+      })
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
         state.user = null;
       });
   },

497404decc11b2eb81156f2dfd70e7542a5a02c2
TakashiNishii <ygortn14@gmail.com>
2024-03-27 17:08:27 -0300

    :art: Login form

--- 12_REACTGRAM/frontend/src/pages/Auth/Login.js
+++ 12_REACTGRAM/frontend/src/pages/Auth/Login.js
@@ -1,7 +1,46 @@
 import "./Auth.css";
 
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target)}
+          value={password || ""}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não tem conta? <Link to="/register">Clique aqui</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;

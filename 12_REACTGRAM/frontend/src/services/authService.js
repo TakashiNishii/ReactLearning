0b1df573a9fb63039a88818dca84667105655248
TakashiNishii <ygortn14@gmail.com>
2024-03-27 17:14:36 -0300

    :wrench: Service de login

--- 12_REACTGRAM/frontend/src/services/authService.js
+++ 12_REACTGRAM/frontend/src/services/authService.js
@@ -24,9 +24,29 @@ const logout = () => {
   localStorage.removeItem("user");
 };
 
+// Sign in an user
+const login = async (data) => {
+  const config = requestConfig("POST", data);
+
+  try {
+    const res = await fetch(api + "/users/login", config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
+    }
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const authService = {
   register,
   logout,
+  login,
 };
 
 export default authService;

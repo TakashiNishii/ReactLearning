52e0980682ed6afd21c8a7d2e9d7d90b9cbf2137
TakashiNishii <ygortn14@gmail.com>
2024-03-30 15:48:51 -0300

    :rocket: Login function

--- 12_REACTGRAM/frontend/src/pages/Auth/Login.js
+++ 12_REACTGRAM/frontend/src/pages/Auth/Login.js
@@ -9,13 +9,29 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 // Redux
-
+import { login, reset } from "../../slices/authSlice";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const user = {
+      email,
+      password,
+    };
+
+    dispatch(login(user));
   };
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, dispatch);
 
   return (
     <div id="login">
@@ -25,16 +41,18 @@ const Login = () => {
         <input
           type="text"
           placeholder="E-mail"
-          onChange={(e) => setEmail(e.target)}
+          onChange={(e) => setEmail(e.target.value)}
           value={email || ""}
         />
         <input
           type="password"
           placeholder="Senha"
-          onChange={(e) => setPassword(e.target)}
+          onChange={(e) => setPassword(e.target.value)}
           value={password || ""}
         />
-        <input type="submit" value="Entrar" />
+        {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message type="error" msg={error} />}
       </form>
       <p>
         Não tem conta? <Link to="/register">Clique aqui</Link>

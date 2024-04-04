2f422c0fc3da76561ba9b0a388a6a04f71eb7df3
TakashiNishii <ygortn14@gmail.com>
2024-04-04 17:21:18 -0300

    :construction: Profile page [base]

--- 12_REACTGRAM/frontend/src/App.js
+++ 12_REACTGRAM/frontend/src/App.js
@@ -15,6 +15,7 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import EditProfile from "./pages/EditProfile/EditProfile";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   const { auth, loading } = useAuth();
@@ -35,6 +36,10 @@ function App() {
             <Route
               path="/profile"
               element={auth ? <EditProfile /> : <Navigate to={"/login"} />}
+            />
+            <Route
+              path="/users/:id"
+              element={auth ? <Profile /> : <Navigate to={"/login"} />}
             />
             <Route
               path="/login"
---
+++ 12_REACTGRAM/frontend/src/pages/Profile/Profile.js
@@ -0,0 +1,21 @@
+import "./Profile.css";
+
+import { uploads } from "../../utils/config";
+
+// components
+import Message from "../../components/Message";
+import { Link } from "react-router-dom";
+import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs";
+
+// hooks
+import { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// redux
+
+const Profile = () => {
+  return <div>Profile</div>;
+};
+
+export default Profile;

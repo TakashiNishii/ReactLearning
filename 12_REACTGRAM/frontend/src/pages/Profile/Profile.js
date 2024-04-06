295119c210138d73e80fef4561b6bfc2759d3e65
TakashiNishii <ygortn14@gmail.com>
2024-04-06 03:39:48 -0300

    :rocket: Show data in profile

--- 12_REACTGRAM/frontend/src/pages/Profile/Profile.js
+++ 12_REACTGRAM/frontend/src/pages/Profile/Profile.js
@@ -13,9 +13,40 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 // redux
+import { getUserDetails } from "../../slices/userSlice";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  const { user, loading } = useSelector((state) => state.user);
+  const { user: userAuth } = useSelector((state) => state.auth);
+
+  // photo
+
+  // Load user data
+  useEffect(() => {
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <div id="profile">
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p>{user.bio} </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;

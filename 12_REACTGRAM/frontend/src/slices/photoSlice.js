527ade9be9fa903f8c65407279c78d3e7c3b5b64
TakashiNishii <ygortn14@gmail.com>
2024-04-06 22:20:38 -0300

    :wrench: Slice insert photo

--- 12_REACTGRAM/frontend/src/slices/photoSlice.js
+++ 12_REACTGRAM/frontend/src/slices/photoSlice.js
@@ -10,7 +10,21 @@ const initialState = {
   message: null,
 };
 
-// Funções
+// Publish user photo
+export const publishPhoto = createAsyncThunk(
+  "photo/publish",
+  async (photo, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await photoService.publishPhoto(photo, token);
+
+    // Check for errors
+    if (data.error) {
+      return thunkAPI.rejectWithValue(data);
+    }
+
+    return data;
+  }
+);
 
 export const photoSlice = createSlice({
   name: "photo",
@@ -19,6 +33,26 @@ export const photoSlice = createSlice({
     resetMessage: (state) => {
       state.message = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(publishPhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(publishPhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photo = action.payload;
+        state.photos.unshift(state.photo);
+        state.message = "Foto publicada com sucesso!";
+      })
+      .addCase(publishPhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = {};
+      });
   },
 });
 

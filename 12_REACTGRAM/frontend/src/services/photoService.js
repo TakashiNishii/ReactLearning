e0a8ffc5a2ee5d79e8f2aa1a25c02fb7ae9714ce
TakashiNishii <ygortn14@gmail.com>
2024-04-07 03:13:07 -0300

    :wrench: Get user photo service

--- 12_REACTGRAM/frontend/src/services/photoService.js
+++ 12_REACTGRAM/frontend/src/services/photoService.js
@@ -14,8 +14,24 @@ const publishPhoto = async (data, token) => {
     console.log(error);
   }
 };
+
+// Get user photos
+const getUserPhotos = async (id) => {
+  const config = requestConfig("GET");
+
+  try {
+    const res = await fetch(api + "/photos/user/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
 const photoService = {
   publishPhoto,
+  getUserPhotos,
 };
 
 export default photoService;

29a8cd26925d52c28b13ec67ac0be33abc41731c
TakashiNishii <ygortn14@gmail.com>
2024-04-09 16:23:40 -0300

    :wrench: Delete photo slice

--- 12_REACTGRAM/frontend/src/slices/photoSlice.js
+++ 12_REACTGRAM/frontend/src/slices/photoSlice.js
@@ -38,6 +38,23 @@ export const getUserPhotos = createAsyncThunk(
   }
 );
 
+// Delete a photo
+export const deletePhoto = createAsyncThunk(
+  "photo/delete",
+  async (id, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.deletePhoto(id, token);
+
+    // Check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -74,6 +91,24 @@ export const photoSlice = createSlice({
         state.success = true;
         state.error = null;
         state.photos = action.payload;
+      })
+      .addCase(deletePhoto.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deletePhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = state.photos.filter((photo) => {
+          return photo._id !== action.payload._id;
+        });
+        state.message = action.payload.message;
+      })
+      .addCase(deletePhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = null;
       });
   },
 });

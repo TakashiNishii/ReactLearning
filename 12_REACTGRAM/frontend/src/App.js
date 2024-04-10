863d29572d62524326e5a4f0106e7a117e14a2c9
TakashiNishii <ygortn14@gmail.com>
2024-04-10 16:13:44 -0300

    :art: Create Individual photo page

--- 12_REACTGRAM/frontend/src/App.js
+++ 12_REACTGRAM/frontend/src/App.js
@@ -16,6 +16,7 @@ import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import EditProfile from "./pages/EditProfile/EditProfile";
 import Profile from "./pages/Profile/Profile";
+import Photo from "./pages/Photo/Photo";
 
 function App() {
   const { auth, loading } = useAuth();
@@ -40,6 +41,10 @@ function App() {
             <Route
               path="/users/:id"
               element={auth ? <Profile /> : <Navigate to={"/login"} />}
+            />
+            <Route
+              path="/photos/:id"
+              element={auth ? <Photo /> : <Navigate to={"/login"} />}
             />
             <Route
               path="/login"
---
+++ 12_REACTGRAM/frontend/src/pages/Photo/Photo.js
@@ -0,0 +1,20 @@
+import "./Photo.css";
+
+import { uploads } from "../../utils/config";
+
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// Redux
+
+const Photo = () => {
+  return <div>Photo</div>;
+};
+
+export default Photo;

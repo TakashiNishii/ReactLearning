d9df2c6465f43b8623b4cae63b7a57ea95e87d3a
TakashiNishii <ygortn14@gmail.com>
2024-04-18 14:39:46 -0400

    :art: Create search page

---
+++ 12_REACTGRAM/frontend/src/pages/Search/Search.js
@@ -0,0 +1,19 @@
+import "./Search.css";
+
+// Hooks
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useResetComponent } from "../../hooks/useResetComponentMessage";
+
+// Components
+import LikeContainer from "../../components/LikeContainer";
+import PhotoItem from "../../components/PhotoItem";
+import { Link } from "react-router-dom";
+
+// Redux
+
+const Search = () => {
+  return <div>Search</div>;
+};
+
+export default Search;
